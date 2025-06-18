91762929241f29fffc3d7988172e9e4c0362400a
indiflex <indiflex1@gmail.com>
2025-06-18 13:39:10 +0900

    mod

---
+++ js/mod/mod1.js
@@ -0,0 +1,4 @@
+import AA, { f1 } from './mod2.js';
+
+console.log(f1());
+console.log(AA(1, 2));
---
+++ js/mod/mod2.js
@@ -0,0 +1,5 @@
+export const f1 = () => 1;
+
+export default function add(a, b) {
+  return a + b;
+}
